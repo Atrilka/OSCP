e8b20daa6abd8fe461b92bd5381cfeda6a76b958
Atrilka <atrilka@gmail.com>
2017-02-06 16:21:24 +0300

    Update cisco.sh

--- cisco.sh
+++ cisco.sh
@@ -2,3 +2,8 @@
 for url in $(cat cisco.txt);do
 host $url |grep "has address" |cut -d" " -f4
 done
+
+OR
+
+for url in $(grep -o '[A-Za-z0-9_\.-]*\.cisco.com' index.html |sort -u); do host $url|grep "has address"|cut -d" " -f4;done
+
